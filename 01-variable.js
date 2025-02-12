801f843fa79cb5c4fce6288c653b76ed516ecf41
sonali0210 <sonalisurpatne02@gmail.com>
2025-02-12 08:38:07 +0530

    01-Variable and Datatype
    Session 01 :Variable and Datatype

---
+++ 01-variable.js
@@ -0,0 +1,9 @@
+var age;  // Variable declaration
+console.log(age);
+
+age="24"; //variable initialization
+console.log("Age:",age);
+
+var presentCity = "Pune"; //declaration and initialization in same line
+console.log("My Present City: ",presentCity);
+
---
+++ 02-datatype.js
@@ -0,0 +1,42 @@
+console.log("====== Primitive Data Type : Number ====== " );
+
+var age = 22;   
+console.log("Age: ",age);
+var typeOfAge = typeof(age);
+console.log("Type of Age: ",typeOfAge);
+
+var weight = 70.50;
+console.log("Weight: ",weight);
+var typeOfWeight = typeof(weight);
+console.log("Type of Weight: ",typeOfWeight);
+
+console.log("====== Primitive Data Type : String ====== ");
+
+var city = "Pune";
+console.log("City: ",city);
+var typeOfCity = typeof(city);
+console.log("Type of City: ",typeOfCity);
+
+var pin = "411015";
+console.log("Pin: ",pin);
+var typeOfPin = typeof(pin);
+console.log("Type of Pin: ",typeOfPin);
+
+console.log("====== Primitive Data Type : Boolean ====== ");
+
+var isMarried = true;
+console.log("Is Married: ",isMarried);
+var typeOfIsMarried = typeof(isMarried);
+console.log("Type of IsMarried: ",typeOfIsMarried);
+
+console.log("====== Special Data Type  ====== ");
+
+var fullName;
+console.log("Full Name: ",fullName); //Before intialization
+var typeOfFullName = typeof(fullName);
+console.log("Type of FullName: ",typeOfFullName); //undefined
+
+var pastAffairs = null;
+console.log("Past Affairs: ",pastAffairs); //Before intialization
+var typeOfPastAffairs = typeof(pastAffairs);
+console.log("Type of PastAffairs: ",typeOfPastAffairs); //object
