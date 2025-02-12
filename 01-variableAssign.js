7819c07ba8a6350568508d8077b43bf328cc379b
sonali0210 <sonalisurpatne02@gmail.com>
2025-02-12 08:49:18 +0530

    01-variableAssign.js
    01-variableAssign.js

---
+++ 01-variableAssign.js
@@ -0,0 +1,32 @@
+var myFullName;
+console.log("My Full Name: ",myFullName);
+
+myFullName = "Sonali Surpatne";
+console.log("My Full Name: ",myFullName);   
+
+myFullName = "Sonali Krushna Surpatne";
+console.log("My Full Name: ",myFullName);
+
+var myLove = "I love only JS";
+console.log("My Love: ",myLove);
+
+var firstName = "Sonali";
+console.log("My First Name: ",firstName);
+
+var lastName = "Surpatne";
+console.log("My Last Name: ",lastName);
+
+var age = 20; 
+console.log("My Age: ",age);
+
+var address = "Sangavi,Pune"; 
+console.log("My Address: ",address);
+
+var pinCode = "411061";
+console.log("My Pin Code: ",pinCode);
+
+var gender = "Female";
+console.log("My Gender: ",gender);
+
+var isMarried = true;
+console.log("Am I Married: ",isMarried);
