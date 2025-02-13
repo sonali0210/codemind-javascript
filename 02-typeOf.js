6c5ce7eebdd6ff06a244a68816532d1e8ff8869b
sonali0210 <sonalisurpatne02@gmail.com>
2025-02-13 11:00:58 +0530

    02-typeOf.js
    02-typeOf.js
    Assignment

---
+++ 02-typeOf.js
@@ -0,0 +1,23 @@
+console.log("********** 1 **********");
+
+var bankName;
+console.log(`Before initialization: ${bankName}  Data Type: ${typeof(bankName)}`);
+bankName = "SBI Bank";
+console.log(`After initialization: ${bankName}  Data Type: ${typeof(bankName)}`);
+
+console.log("********** 2 **********");
+
+var marks = '90%';
+console.log(`Marks: ${marks}  Data Type: ${typeof(marks)}`);
+
+console.log("********** 3 **********");
+var isWorking;
+console.log(`Before initialization: ${isWorking}  Data Type: ${typeof(isWorking)}`);
+isWorking = true;
+console.log(`After initialization: ${isWorking}  Data Type: ${typeof(isWorking)}`);
+
+console.log("********** 4 **********");
+var totalCount = "Hundred and Seven";
+console.log(`Before : ${totalCount}  Data Type: ${typeof(totalCount)}`);
+var totalCount = 107;
+console.log(`After : ${totalCount}  Data Type: ${typeof(totalCount)}`);
