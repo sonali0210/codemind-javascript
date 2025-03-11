569e1509f375b943e5de10ad7e06b44a30e604cb
sonali0210 <sonalisurpatne02@gmail.com>
2025-03-11 17:53:25 +0530

    domAPIAssignment
    domAPIAssignment

---
+++ 27_main.js
@@ -0,0 +1,8 @@
+const profDetails = document.querySelector("#profileDetails");
+profDetails.style.color = "coral";
+
+const techStack = document.querySelector("#techStack");
+techStack.style.color = "blue";
+
+const list = document.querySelector("#list");
+list.style.color = "indigo";
